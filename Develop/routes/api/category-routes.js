5c4d0ace8b2106c86da1ab2223b085e6904da39d
BryanGC96 <2014104@icum.edu.mx>
2024-04-03 17:20:10 -0600

    Added router get / , router get /:id, inside category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,16 +1,40 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }]
+    });
+
+    res.status(200).json(categories);
+  } catch (error) {
+    console.error('Error fetching categories:', error); //TENGO QUE BORRAR ESTE MUGREROOOO!!!!!
+    res.status(500).json({ error: 'Error fetching categories' });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryId = req.params.id;
+    const category = await Category.findByPk(categoryId, {
+      include: [{ model: Product }]
+    });
+    if (category) {
+      res.status(200).json(category);
+    } else {
+      res.status(404).json({ error: 'Category not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching category:', error);
+    res.status(500).json({ error: 'Error fetching category'});
+  }
 });
 
 router.post('/', (req, res) => {
