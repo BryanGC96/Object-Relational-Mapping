5de0354ba6ec41e59c07e4c1e6af7cd419dbbcb3
BryanGC96 <2014104@icum.edu.mx>
2024-04-04 13:00:15 -0600

    Added logic to the 'tag-routes.js' file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 
     res.status(200).json(products);
   } catch (error) {
-    console.error('Error fetching products:', error);
+    console.error('Error fetching products:', error); // Borrrrrar/////////////////////////////////
     res.status(500).json({ error: 'Error fetching products' });
   }
 });
@@ -135,7 +135,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ error: 'Product not found' });
     }
   } catch (error) {
-    console.error('Error deleting product:' error);
+    console.error('Error deleting product:' error); // Borrrrrar/////////////////////////////////
     res.status(500).json({ error: 'Error deleting product' });
   }
 });
